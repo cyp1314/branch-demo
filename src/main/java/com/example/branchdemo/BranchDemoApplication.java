10e8a3a43799d2a01b71fff466d627e81c1083cb
cyp1314 <xj.chenyongpeng@gmail.com>
2020-08-31 18:31:54 +0800

    Initial master commit

---
+++ src/main/java/com/example/branchdemo/BranchDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.branchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class BranchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BranchDemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello(){
+        return "master,hello world!";
+    }
+
+}
---
+++ src/test/java/com/example/branchdemo/BranchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.branchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
